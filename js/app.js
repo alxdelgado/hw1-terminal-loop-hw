a8485eae06b88392af7e9f4c637523e570a630b4
Alex Delgado <delgadoalex@icloud.com>
2018-07-24 21:16:35 -0500

    Section 4 complete

--- js/app.js
+++ js/app.js
@@ -9,19 +9,58 @@
   // You can use a for loop to write DRY code as an example. 
 
 // SECTION 2 // 
-// 1. a < b; 
-// 2. c > d; 
-// 3. 'name' === 'name'; 
-// 4. a < b < c;
-// 5. a__a__d; 
-// 6. e = 'kevin'; 
-// 7. 48 = '48'; 
+const a = 4; 
+const b = 53; 
+const c = 57; 
+const d = 16; 
+const e = 'Kevin'; 
+
+console.log(a < b); 
+console.log( c > d); 
+console.log('name' === 'name'); 
+console.log(a < b < c); 
+console.log(a * a = d); 
+console.log(e === 'Kevin');
+console.log(48 === '48'); 
 
 
 // SECTION 3 // 
 // 1. This is an infinite loop, there is no value set to end the loop. 
 // 2. No, because runProgram is set = false. 
 // 3. The code gave me the predicted response of 'AAAAAAAAAAAAAAA', as long as 'i' is < 20 the while loop will continue to pront the letters 'A'. 
+
+
+// SECTION 4 //
+// For Loop
+// A for loop runs a selected number of iterations. A while loop allows code to be run repeatedly based on a given boolean condition. 
+
+// For Loop I // 
+for(let k = 0; k < 1000; k++){
+  console.log(k); 
+} 
+
+// For Loop Control Statement // 
+// The first part of a control statement is the intializer. 
+// The second part of the control statement is the comparison. 
+// The third part of the control statement is the switch. 
+
+// For Loop in reverse // 
+for(let e = 999; e >= 0; e--){
+  console.log(e); 
+}
+
+// More Counting // 
+for (let i = 0; i < 10; i++){
+  console.log('the value of i is: ' + i + ' of 10');
+}
+
+
+
+
+
+
+
+
 
 
 
