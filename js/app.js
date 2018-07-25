966b2dcf9239f389fd8c689390d10f14e572463c
Alex Delgado <delgadoalex@icloud.com>
2018-07-24 20:05:47 -0500

    Section 1 modified

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 // Homework Terminal Loop // 
 
-// 1. 
+// 1. String interpolation is a way to construct a new String value from a mix of constants, variables, literals, and expressions"
+  'this is string interpolation'
+  // "Concatenation allows you to combine to strings together."
+  const newString = 'stringA' + 'stringB'
+
+// 2. DRY code is to reduce repitition of code. We should pay attention to it because we can reduce the amount of code we write by using certain loops or other methods to do more than one task.
+  // You can use a for loop to write DRY code as an example. 
+
+// SECTION 2 // 
+
